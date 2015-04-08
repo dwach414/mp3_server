30b7cbba053ca142eb29b6d9eeda746365aa0bf2
Daniel Wachtel <dwach414@gmail.com>
2015-04-08 01:04:22 -0500

    for testing

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ tasks_id_route.put(function(req, res){
         task.deadline = req.body.deadline;
         console.log(req.body);
         console.log(req.body.assignedUser == null);
+        console.log(req.body.assignedUser == undefined);
         console.log('assignedUser' in req.body);
         console.log('description' in req.body);
         if(req.body.description) task.description = req.body.description;
