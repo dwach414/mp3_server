3836b1b5eaca2fbb27dcca11e9dcb2c9f8386b69
Daniel Wachtel <dwach414@gmail.com>
2015-04-10 01:58:59 -0500

    testing

--- server.js
+++ server.js
@@ -144,6 +144,7 @@ users_id_route.put(function(req, res){
   if('pendingTasks' in req.body){
     console.log("woo");
   }
+  console.log(req.body);
   var args = ('pendingTasks' in req.body) ? {name: req.body.name, email: req.body.email, pendingTasks: req.body.pendingTasks} : {name: req.body.name, email: req.body.email};
   User.findByIdAndUpdate(req.params.id, args, function(err, user){
     if (err || user == null) {
